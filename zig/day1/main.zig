b74b379391b50bf9d4df7955b55653455f27ff7e
Julien FRANCAIS <jfrancai@student.42.fr>
2023-07-03 17:17:20 +0200

    zig day1 2d star

--- zig/day1/main.zig
+++ zig/day1/main.zig
@@ -7,8 +7,6 @@ const expect = std.testing.expect;
 
 const File = std.fs.File;
 
-const skip: bool = false;
-
 fn nextLine(reader: anytype, buffer: []u8) !?[]const u8 {
     var line = (try reader.readUntilDelimiterOrEof(
         buffer,
@@ -17,10 +15,7 @@ fn nextLine(reader: anytype, buffer: []u8) !?[]const u8 {
     return line;
 }
 
-test "Find Solution" {
-    if (skip)
-        return error.SkipZigTest;
-
+test "Part1" {
     const flags: File.OpenFlags = .{};
     const file = try std.fs.cwd().openFile("./input", flags);
     defer file.close();
@@ -46,4 +41,33 @@ test "Find Solution" {
         }
     }
     std.debug.print("Here {d} \n", .{std.mem.max(u32, list.items)});
+}
+
+test "Part2" {
+    const flags: File.OpenFlags = .{};
+    const file = try std.fs.cwd().openFile("./input", flags);
+    defer file.close();
+
+    var buffer: [100]u8 = undefined;
+
+    var list = ArrayList(u32).init(test_allocator);
+    defer list.deinit();
+
+    try list.append(0);
+
+    while (true) {
+        const input = (try nextLine(file.reader(), &buffer));
+        if (input) |i| {
+            if (i.len == 0) {
+                try list.append(0);
+            } else {
+                const number = try std.fmt.parseInt(u32, i, 10);
+                list.items[list.items.len - 1] += number;
+            }
+        } else {
+            break;
+        }
+    }
+    std.sort.sort(u32, list.items, {}, comptime std.sort.desc(u32));
+    std.debug.print("Here {d} \n", .{list.items[0] + list.items[1] + list.items[2]});
 }
