46adf1af223a10688fb4d7e8e020a771eb9984ec
Julien FRANCAIS <jfrancai@student.42.fr>
2023-07-18 11:57:01 +0200

    day1

--- ts/src/day1/main.ts
+++ ts/src/day1/main.ts
@@ -1,3 +1,37 @@
-const solution = () => {};
+import fs from "fs";
+
+const solution = () => {
+  fs.readFile(
+    `${__dirname}/../../../subjects/day1/input`,
+    "utf8",
+    (err, data) => {
+      if (err) {
+        console.log(err);
+        return;
+      }
+      const parsedInput = data.split("\n");
+
+      let result = 0;
+
+      parsedInput.reduce(
+        (accumulator: number | undefined, currentValue: string) => {
+          if (accumulator === undefined) {
+            accumulator = 0;
+          } else if (currentValue !== "") {
+            accumulator += parseInt(currentValue);
+          } else if (currentValue === "") {
+            if (result < accumulator) {
+              result = accumulator;
+            }
+            accumulator = 0;
+          }
+          return accumulator;
+        },
+        0
+      );
+      console.log(result);
+    }
+  );
+};
 
 export default { solution };
